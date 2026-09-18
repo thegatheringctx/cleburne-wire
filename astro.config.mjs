37db74e5a3dedee6c5819cb51a2beb1402943054
thegatheringctx <pastorbillyphilips@gmail.com>
2026-09-18 10:02:50 -0500

    Add Astro configuration for Netlify deployment

---
+++ astro.config.mjs
@@ -0,0 +1,3 @@
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
+export default defineConfig({ output: "server", adapter: netlify(), site: "https://cleburnewire.com" });
